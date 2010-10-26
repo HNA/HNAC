27fb750e91bbfde5d8a994ecd8be3860d5cd9dca
Gangs <gangs1989@gmail.com>
2010-10-26 15:02:47 +0300

    Изменения интерфейса

--- HnacConfig/mainwindow.cpp
+++ HnacConfig/mainwindow.cpp
@@ -4,8 +4,8 @@
 #include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+        QMainWindow(parent),
+        ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     setFixedSize(320, 280);
@@ -19,30 +19,33 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->pushButton_loginsave->setVisible(false);
     ui->pushButton_passsave->setVisible(false);
     if(control->isInstalled())
-    {
-        ui->pushButton_install->setVisible(false);
-        ui->pushButton_uninstall->setVisible(true);
-        if(control->isRunning())
         {
-            ui->pushButton_start->setVisible(false);
-            ui->pushButton_restart->setVisible(true);
-            ui->pushButton_stop->setVisible(true);
+            ui->statusBar->showMessage(tr("Hnac service already instaled. Press \"Start\""));
+            ui->pushButton_install->setVisible(false);
+            ui->pushButton_uninstall->setVisible(true);
+            if(control->isRunning())
+                {
+                    ui->statusBar->showMessage(tr("Hnac service is running"));
+                    ui->pushButton_start->setVisible(false);
+                    ui->pushButton_restart->setVisible(true);
+                    ui->pushButton_stop->setVisible(true);
+                }
+            else
+                {
+                    ui->pushButton_start->setVisible(true);
+                    ui->pushButton_restart->setVisible(false);
+                    ui->pushButton_stop->setVisible(false);
+                }
         }
-        else
+    else
         {
-            ui->pushButton_start->setVisible(true);
+            ui->statusBar->showMessage(tr("Press \"Install\" to install service"));
+            ui->pushButton_install->setEnabled(true);
+            ui->pushButton_uninstall->setVisible(false);
+            ui->pushButton_start->setVisible(false);
             ui->pushButton_restart->setVisible(false);
             ui->pushButton_stop->setVisible(false);
         }
-    }
-    else
-    {
-        ui->pushButton_install->setEnabled(true);
-        ui->pushButton_uninstall->setVisible(false);
-        ui->pushButton_start->setVisible(false);
-        ui->pushButton_restart->setVisible(false);
-        ui->pushButton_stop->setVisible(false);
-    }
 
 }
 
@@ -53,61 +56,83 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_start_clicked()
 {
-    control->start();
-    ui->pushButton_start->setVisible(false);
-    ui->pushButton_restart->setVisible(true);
-    ui->pushButton_stop->setVisible(true);
+    if(control->start())
+        {
+            ui->statusBar->showMessage(tr("Hnac service is running"));
+            ui->pushButton_start->setVisible(false);
+            ui->pushButton_restart->setVisible(true);
+            ui->pushButton_stop->setVisible(true);
+        }
 }
 
 void MainWindow::on_pushButton_stop_clicked()
 {
-    control->stop();
-    ui->pushButton_start->setVisible(true);
-    ui->pushButton_stop->setVisible(false);
-    ui->pushButton_restart->setVisible(false);
+    if(control->stop())
+        {
+            ui->statusBar->showMessage(tr("Hnac service is stopped"));
+            ui->pushButton_start->setVisible(true);
+            ui->pushButton_stop->setVisible(false);
+            ui->pushButton_restart->setVisible(false);
+        }
 }
 
 void MainWindow::on_pushButton_restart_clicked()
 {
-    control->stop();
-    control->start();
+    if(control->stop())
+        {
+            ui->statusBar->showMessage(tr("Hnac service is stopped"));
+            ui->pushButton_start->setVisible(true);
+            ui->pushButton_stop->setVisible(false);
+            ui->pushButton_restart->setVisible(false);
+            if(control->start())
+                {
+                    ui->statusBar->showMessage(tr("Hnac service is running"));
+                    ui->pushButton_start->setVisible(false);
+                    ui->pushButton_restart->setVisible(true);
+                    ui->pushButton_stop->setVisible(true);
+                }
+        }
 }
 
 void MainWindow::on_pushButton_cancel_clicked()
 {
-   close();
+    close();
 }
 
 void MainWindow::on_pushButton_install_clicked()
 {
     if(QtServiceController::install(qApp->applicationDirPath() + "\\"  + "hnac.exe"))
         {
+            ui->statusBar->showMessage(tr("Hnac service is instaled. Press \"Start\""));
             ui->pushButton_start->setVisible(true);
             ui->pushButton_install->setVisible(false);
             ui->pushButton_uninstall->setVisible(true);
         }
     else
         {
-            QMessageBox::critical(this, "Hna Client Error", "An error occurred during installation");
+            ui->statusBar->showMessage(tr("An error occurred during installation"));
         }
 }
 
 void MainWindow::on_pushButton_uninstall_clicked()
 {
-    control->uninstall();
-    ui->pushButton_install->setVisible(true);
-    ui->pushButton_uninstall->setVisible(false);
-    ui->pushButton_start->setVisible(false);
-    ui->pushButton_stop->setVisible(false);
-    ui->pushButton_restart->setVisible(false);
+    if(control->uninstall())
+        {
+            ui->statusBar->showMessage(tr("Hnac service is uninstaled"));
+            ui->pushButton_install->setVisible(true);
+            ui->pushButton_uninstall->setVisible(false);
+            ui->pushButton_start->setVisible(false);
+            ui->pushButton_stop->setVisible(false);
+            ui->pushButton_restart->setVisible(false);
+        }
 }
 
 void MainWindow::on_lineEdit_login_textChanged(QString text)
 {
     if(text != login)
-    {
-          ui->pushButton_loginsave->setVisible(true);
-    }
+        {
+            ui->pushButton_loginsave->setVisible(true);
+        }
     else
         {
             ui->pushButton_loginsave->setVisible(false);
@@ -117,9 +142,9 @@ void MainWindow::on_lineEdit_login_textChanged(QString text)
 void MainWindow::on_lineEdit_password_textChanged(QString text)
 {
     if(text != pass)
-    {
-        ui->pushButton_passsave->setVisible(true);
-    }
+        {
+            ui->pushButton_passsave->setVisible(true);
+        }
     else
         {
             ui->pushButton_passsave->setVisible(false);
@@ -129,19 +154,20 @@ void MainWindow::on_lineEdit_password_textChanged(QString text)
 void MainWindow::on_checkBox_showpass_clicked()
 {
     if(ui->checkBox_showpass->isChecked())
-    {
-        ui->lineEdit_password->setEchoMode(QLineEdit::Normal);
-    }
+        {
+            ui->lineEdit_password->setEchoMode(QLineEdit::Normal);
+        }
     else
-    {
-        ui->lineEdit_password->setEchoMode(QLineEdit::Password);
-    }
+        {
+            ui->lineEdit_password->setEchoMode(QLineEdit::Password);
+        }
 }
 
 void MainWindow::on_pushButton_loginsave_clicked()
 {
     login = ui->lineEdit_login->text();
     settings->setValue("login", login);
+    ui->statusBar->showMessage(tr("Login saved"));
     ui->pushButton_loginsave->setVisible(false);
 }
 
@@ -150,5 +176,6 @@ void MainWindow::on_pushButton_passsave_clicked()
 {
     pass = ui->lineEdit_password->text();
     settings->setValue("password", pass);
+    ui->statusBar->showMessage(tr("Password saved"));
     ui->pushButton_passsave->setVisible(false);
 }
